b8e6e03b05d2650000cb31ac2d013a91f43d48aa
Rafael Sánchez <derick290801@gmail.com>
2024-09-11 21:05:07 -0400

    add event to the button

--- src/main.js
+++ src/main.js
@@ -1,1 +1,23 @@
-console.log('Hola mundo');
+let AddTodo = function() {
+    //get search bar text
+    const searchBar = document.getElementById("search_bar");
+    console.log(searchBar.innerHTML);
+    searchBar.innerHTML = 'hola mundo';
+
+    //add it to the list
+
+    // alert(searchBar.innerText);
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    console.log('DOM completed')
+});
+
+const submitButton = document.querySelector('#submit_button');
+submitButton.addEventListener('click', function() {
+    alert(submitButton?.value);
+});
+
+console.log(submitButton);
+
+
